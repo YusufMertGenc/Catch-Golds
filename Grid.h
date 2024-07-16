ff0eb328c377007d3c567a37dddcb6ee96d59575
mert <yusufmertgencc@gmail.com>
2024-07-16 21:26:20 +0300

    update

---
+++ Grid.h
@@ -0,0 +1,54 @@
+//
+// This file generates the board
+//
+
+#ifndef ASSIGNMENT3_GRID_H
+#define ASSIGNMENT3_GRID_H
+
+#include <ctime>
+#include <iostream>
+
+
+class Grid {
+
+private:
+    int size;
+    char grid[100][100];//This is the grid which has the elements
+    char HidedGrid[100][100];//This is the grid which is hided
+
+public:
+
+    Grid(int size){//Constructor for Grid
+
+        srand(time(NULL));
+
+        this->size = size;
+
+        for (int i = 0; i < size ; i++){
+            for (int j = 0; j<size; j++){
+
+                grid[i][j] = '.';
+            }
+        }
+        for (int i = 0; i < size ; i++){
+            for (int j = 0; j<size; j++){
+
+                HidedGrid[i][j] = '.';
+            }
+        }
+
+
+
+    }
+
+    void hidedGrid (int x,int y,char foundChar);
+    void displayCurrentGrid();
+    void elementDeploy (int numberOfElements);
+    void display();
+    char choice (int x, int y);
+
+
+};
+
+
+#endif //ASSIGNMENT3_GRID_H
---
+++ Player.h
@@ -0,0 +1,52 @@
+//
+//
+//
+
+#ifndef ASSIGNMENT3_PLAYER_H
+#define ASSIGNMENT3_PLAYER_H
+#include <iostream>
+#include "Element.h"
+
+class Player {
+
+private:
+    int score;
+    int goldCount;
+    int health;
+    int boardSize;
+
+public:
+
+    Player(int boardSize){ //parametic constructor
+        this->boardSize = boardSize;
+        this->health = 2*boardSize; //according to instructor
+
+        score = 0; //initialy our score is 0
+
+        this->goldCount = 0;
+    }
+
+
+
+    void goldCounter ();
+
+    void adjustHealth (int adjust);
+
+    int getScore() const;
+
+    void setScore(int score);
+
+    int getGoldCount() const;
+
+    void setGoldCount(int goldCount);
+
+    int getHealth();
+
+    void setHealth(int health);
+
+    int getBoardSize ();
+
+};
+
+
+#endif //ASSIGNMENT3_PLAYER_H
